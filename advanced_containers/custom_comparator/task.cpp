aebcbc1ee700d8f20c9dd339dda3e620b61f62cc
Gregory Uklein <73195672+grishanchel@users.noreply.github.com>
2024-06-04 18:05:46 +0300

    Add files via upload

---
+++ advanced_containers/custom_comparator/task.cpp
@@ -0,0 +1,18 @@
+#include "task.h"
+#include "iostream" 
+
+using namespace std;
+
+bool operator <(const SuperKey& lhs, const SuperKey& rhs) {
+    return lhs.StrPart<=rhs.StrPart;
+}
+
+void PopulateMap(
+    std::map<SuperKey, std::string>& map,
+    const std::vector<std::pair<SuperKey, std::string>>& toAdd
+) {
+
+    for (std::pair<SuperKey, std::string> it :  toAdd) {
+        map[it.first] = it.second;
+    }
+}
